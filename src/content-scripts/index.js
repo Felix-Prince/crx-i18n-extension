3c05ce6c1287144c782435c92c1576c552c27d45
PengfeiZhu <zhupengfei@caicloud.io>
2020-07-24 17:44:49 +0800

    增加页面弹出框上的保存按钮

--- src/content-scripts/index.js
+++ src/content-scripts/index.js
@@ -13,6 +13,24 @@ function createPanel() {
 		<div class="Translation_tlContainer__2Jt2_">
 			<div class="Translation_tlHeader__29IRr">
 				<h2>翻译</h2>
+				<button type="button" class="ant-btn">
+				<i aria-label="icon: save" class="anticon anticon-save">
+					<svg
+						viewBox="64 64 896 896"
+						focusable="false"
+						class=""
+						data-icon="save"
+						width="1em"
+						height="1em"
+						fill="currentColor"
+						aria-hidden="true"
+					>
+						<path
+							d="M893.3 293.3L730.7 130.7c-7.5-7.5-16.7-13-26.7-16V112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V338.5c0-17-6.7-33.2-18.7-45.2zM384 184h256v104H384V184zm456 656H184V184h136v136c0 17.7 14.3 32 32 32h320c17.7 0 32-14.3 32-32V205.8l136 136V840zM512 442c-79.5 0-144 64.5-144 144s64.5 144 144 144 144-64.5 144-144-64.5-144-144-144zm0 224c-44.2 0-80-35.8-80-80s35.8-80 80-80 80 35.8 80 80-35.8 80-80 80z"
+						></path>
+					</svg>
+				</i>
+				<span>保存</span>
 			</div>
 			<div class="Translation_tlBody__1SOcI">
 				<div>
