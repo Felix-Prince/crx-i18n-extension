8e6d68cb216b70cb9287c6a1cd8fe5188e3dc4ac
PengfeiZhu <zhupengfei@caicloud.io>
2020-07-28 18:53:34 +0800

    eject 后的多入口

---
+++ src/option/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Translation from "../components/Translation";
+
+ReactDOM.render(<Translation />, document.getElementById("root"));
